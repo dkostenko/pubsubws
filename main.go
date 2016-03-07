0fd9b78d9ccc1fc1fc4590b75080065428944263
forapp <forapp@forapps-Air.Dlink>
2016-03-07 12:51:03 +0300

    add simple server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "your request!")
+}
 
 func main() {
-	fmt.Println("Hi user!")
+	fmt.Println("Run simple server")
+
+	http.HandleFunc("/", sayhelloName)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
